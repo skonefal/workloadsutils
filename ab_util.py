d8b71675b7317348fcb467076aee85a84fe1c7d8
Szymon Konefal <szymon.konefal@intel.com>
2015-06-17 15:00:37 +0200

    fix typo

--- ab_util.py
+++ ab_util.py
@@ -7,6 +7,8 @@ STRESS_ITERATION_TIME = 10 * 60
 
 STRESS_LEVELS = [2,10,25,50,75,100]
 
+# ENDPOINTS = ["http://10.102.44.201/index.php/Special:Random", "http://10.102.44.202/index.php/Special:Random", "http://10.102.44.203/index.php/Special:Random"]
+
 def do_stress():
     print("{0}: Starting idle time for {1} seconds".format(datetime.datetime.now(), IDLE_TIME))
     time.sleep(IDLE_TIME)
@@ -14,7 +16,7 @@ def do_stress():
         Timestamp = datetime.datetime.now()
         print("{0}: Starting stress level {1} for {2} secs".format(
             datetime.datetime.now(), stress_level, STRESS_ITERATION_TIME))
-        os.system("ab -c {0} -s {1}-n 900000 -l -r http://10.102.44.202/index.php/Special:Random".format(
+        os.system("ab -c {0} -s {1} -n 900000 -l -r http://10.102.44.202/index.php/Special:Random".format(
             stress_level, STRESS_ITERATION_TIME))
         pass
     

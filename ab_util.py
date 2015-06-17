8471a7d0d9fd57297d4f382409503d038a79d761
Szymon Konefal <szymon.konefal@intel.com>
2015-06-17 14:33:29 +0200

    stress makers

---
+++ ab_util.py
@@ -0,0 +1,26 @@
+import os
+import datetime
+import time
+
+IDLE_TIME = 2 * 60 
+STRESS_ITERATION_TIME = 10 * 60
+
+STRESS_LEVELS = [2,10,25,50,75,100]
+
+def do_stress():
+    print("{0}: Starting idle time for {1} seconds".format(datetime.datetime.now(), IDLE_TIME))
+    time.sleep(IDLE_TIME)
+    for stress_level in STRESS_LEVELS:
+        Timestamp = datetime.datetime.now()
+        print("{0}: Starting stress level {1} for {2} secs".format(
+            datetime.datetime.now(), stress_level, STRESS_ITERATION_TIME))
+        os.system("ab -c {0} -s {1}-n 900000 -l -r http://10.102.44.202/index.php/Special:Random".format(
+            stress_level, STRESS_ITERATION_TIME))
+        pass
+    
+    print("{0}: Stress finished after {1} iterations".format(
+        datetime.datetime.now(), len(STRESS_LEVELS)))
+    return
+
+if __name__ == '__main__':
+    do_stress()
---
+++ stress_util.py
@@ -0,0 +1,27 @@
+import datetime
+import os
+import time
+
+IDLE_TIME = 10 * 60 
+STRESS_ITERATION_TIME = 10 * 60
+STRESS_LEVEL_BEGINING = 1 # stress level in the begining of test
+STRESS_ITER_INCREMENT = 2 #in each iteration stress will increase X times
+NUM_OF_ITERATIONS = 4
+
+def do_stress():
+    print("{0}: Starting idle time for {1} seconds".format(datetime.datetime.now(), IDLE_TIME))
+    time.sleep(IDLE_TIME)
+    cpu_stress_level = STRESS_LEVEL_BEGINING
+    for iter in range(0, NUM_OF_ITERATIONS):
+        print("{0}: Starting stress level {1} for {2} secs | Iteration #{3}".format(
+            datetime.datetime.now(), cpu_stress_level, STRESS_ITERATION_TIME, iter))
+        os.system("stress -c {0} -t {1}".format(cpu_stress_level, STRESS_ITERATION_TIME))
+        cpu_stress_level = cpu_stress_level * STRESS_ITER_INCREMENT
+        time.sleep(STRESS_ITERATION_TIME)
+        pass
+    
+    print("{0}: Stress finished after {1} iterations".format(datetime.datetime.now(), NUM_OF_ITERATIONS))
+    return
+
+if __name__ == '__main__':
+    do_stress()
